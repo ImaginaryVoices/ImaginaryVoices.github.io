c5d4626616d60e7bc3a943817b015c5d0526fd26
F <48335734+ImaginaryVoices@users.noreply.github.com>
2021-08-15 11:49:06 -0500

    Update index_map.js

--- index_map.js
+++ index_map.js
@@ -242,7 +242,7 @@ legendB.onAdd = function (map) {
     div.innerHTML += '<b>Cambio en <br>rendimiento</b><br>';
     div.innerHTML += '<i style= "background: '+'rgba(255,255,255,0.0)'+' "></i>' + '<em>Incremento</em><br>';
     for (var i = 0; i < grades.length; i++) {
-        div.innerHTML += '<i salign="right" style=" background:'+getColorB(grades[i])+' "></i>' + '<br>';
+        div.innerHTML += '<i align="right" style=" background:'+getColorB(grades[i])+' "></i>' + '<br>';
     }
     div.innerHTML += '<i style=" background:'+'rgba(255,255,255,0.0)'+' "></i>' + '<em>Decremento</em><br>';
     return div;
