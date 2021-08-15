98ca09aff8137e72c6bd5ef5edd3872937a9bf4b
F <48335734+ImaginaryVoices@users.noreply.github.com>
2021-08-15 10:46:55 -0500

    Update index_map.js

--- index_map.js
+++ index_map.js
@@ -237,7 +237,7 @@ legendA.onAdd = function (map) {
 legendB.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'info legend'),
         grades = [0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4],
-        labels = ['>30 %', '20 — 30 %', '10 — 20 %', '&ensp;&nbsp;0 — 10 %', '&ensp;&ensp;0 — -10 %', '-10 — -20 %', '-20 — -30 %', '<-30 %'];
+        labels = ['> 30 %', '20 — 30 %', '10 — 20 %', '&nbsp;&nbsp;&nbsp;0 — 10 %', '&ensp;&nbsp;&nbsp;0 — -10 %', '-10 — -20 %', '-20 — -30 %', '< -30 %'];
 
     div.innerHTML += '<b>Cambio en <br>rendimiento</b><br>';
     div.innerHTML += '<i style= "background: '+'rgba(255,255,255,0.0)'+' "></i>' + '<em>Incremento</em><br>';
