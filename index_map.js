8ff004664dadde12696caf17d72e1478d4eb1404
F <48335734+ImaginaryVoices@users.noreply.github.com>
2021-08-20 23:40:17 -0500

    Update index_map.js

--- index_map.js
+++ index_map.js
@@ -20,7 +20,7 @@ var sidebar = L.control.sidebar('sidebar').addTo(map);
 
 //<!------ Ventana con mensaje de inicio ------>
 var contenu = {
-    content:'En la barra de la izquierda encontrarás más información sobre este mapa interactivo. Selecciona:<br> &emsp;<i class="fa fa-line-chart"></i> para consultar un breve análisis de los datos.<br> &emsp;<i class="fa fa-info"></i> para consultar la descripción de las variables desplegadas. <br><br><small>Pronóstico emitido el 13.08.2021.</small>'
+    content:'En la barra de la izquierda encontrarás más información sobre este mapa interactivo. Selecciona:<br> &emsp;<i class="fa fa-line-chart"></i> para consultar un breve análisis de los datos.<br> &emsp;&nbsp;<i class="fa fa-info"></i> para consultar la descripción de las variables desplegadas. <br><br><small>~Pronóstico emitido el 13.08.2021~</small>'
 };
 if (window.screen.width <= 768) { // Que no aparezca en celulares
     contenu.content = '<small><i>ADVERTENCIA: Este mapa aún no es 100 % compatible con la resolución de un teléfono. Si tienes problemas para visualizarlo, intenta abrirlo desde un ordenador o una tableta.</i></small><br><br>'
@@ -30,7 +30,7 @@ var winOpts = L.control.window(map, {
     title:'¡BIENVENIDO!',
     ...contenu,
     modal: true,
-    maxWidth: 500,
+    maxWidth: 450,
     visible: true
 }).prompt({callback:function(){}})
 
