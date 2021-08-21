8b89077779df60353350832a95b8b8c4ba05fa27
F <48335734+ImaginaryVoices@users.noreply.github.com>
2021-08-20 19:05:11 -0500

    qwe

--- index_map.js
+++ index_map.js
@@ -20,7 +20,7 @@ var sidebar = L.control.sidebar('sidebar').addTo(map);
 
 //<!------ Ventana con mensaje de inicio ------>
 var contenu = {
-    content:'En la barra de la izquierda encontrarás más información sobre este mapa interactivo:<br> &emsp;Selecciona "i" para consultar la descripción de las variables desplegadas.<br> &emsp;Elige el ícono del "gráfico" para consultar un breve análisis de los datos.<br><br><small>Pronóstico emitido el 13.08.2021.</small>'
+    content:'En la barra de la izquierda encontrarás más información sobre este mapa interactivo. Selecciona "i" para consultar la descripción de las variables desplegadas. Elige el ícono del "gráfico" para consultar un breve análisis de los datos.<br><br><small>Pronóstico emitido el 13.08.2021.</small>'
 };
 if (window.screen.width <= 768) { // Que no aparezca en celulares
     contenu.content = '<small><i>ADVERTENCIA: Este mapa aún no es 100 % compatible con la resolución de un teléfono. Si tienes problemas para visualizarlo, intenta abrirlo desde un ordenador o una tableta.</i></small><br><br>'
